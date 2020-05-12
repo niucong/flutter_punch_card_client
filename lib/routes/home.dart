b6ea792df0fb651da755956e802e69cb34e4b372
牛聪 <niucong@julong.cc>
2020-05-12 11:06:51 +0800

    添加百度定位

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,3 +1,9 @@
+import 'dart:async';
+
+import 'package:bdmap_location_flutter_plugin/bdmap_location_flutter_plugin.dart';
+import 'package:bdmap_location_flutter_plugin/flutter_baidu_location.dart';
+import 'package:bdmap_location_flutter_plugin/flutter_baidu_location_android_option.dart';
+import 'package:bdmap_location_flutter_plugin/flutter_baidu_location_ios_option.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,7 +17,13 @@ class HomeRoute extends StatefulWidget {
 }
 
 class _HomeRouteState extends State<HomeRoute> {
-//  LocationFlutterPlugin _locationPlugin = new LocationFlutterPlugin();
+  // 百度定位
+  Map<String, Object> _loationResult;
+  BaiduLocation _baiduLocation;
+  StreamSubscription<Map<String, Object>> _locationListener;
+  LocationFlutterPlugin _locationPlugin = new LocationFlutterPlugin();
+
+  // 极光推送
   String debugLable = 'Unknown';
   final JPush jpush = new JPush();
 
@@ -19,6 +31,82 @@ class _HomeRouteState extends State<HomeRoute> {
   void initState() {
     super.initState();
     initPlatformState();
+
+    /// 动态申请定位权限
+    _locationPlugin.requestPermission();
+
+    /// 设置ios端ak, android端ak可以直接在清单文件中配置
+//    LocationFlutterPlugin.setApiKey("百度地图开放平台申请的ios端ak");
+
+    _locationListener =
+        _locationPlugin.onResultCallback().listen((Map<String, Object> result) {
+      setState(() {
+        _loationResult = result;
+        try {
+          _baiduLocation = BaiduLocation.fromMap(result);
+//          print(_baiduLocation);
+        } catch (e) {
+          print(e);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (null != _locationListener) {
+      _locationListener.cancel();
+    }
+  }
+
+  /// 设置android端和ios端定位参数
+  void _setLocOption() {
+    /// android 端设置定位参数
+    BaiduLocationAndroidOption androidOption = new BaiduLocationAndroidOption();
+    androidOption.setCoorType("bd09ll"); // 设置返回的位置坐标系类型
+    androidOption.setIsNeedAltitude(true); // 设置是否需要返回海拔高度信息
+    androidOption.setIsNeedAddres(true); // 设置是否需要返回地址信息
+    androidOption.setIsNeedLocationPoiList(true); // 设置是否需要返回周边poi信息
+    androidOption.setIsNeedNewVersionRgc(true); // 设置是否需要返回最新版本rgc信息
+    androidOption.setIsNeedLocationDescribe(true); // 设置是否需要返回位置描述
+    androidOption.setOpenGps(true); // 设置是否需要使用gps
+    androidOption.setLocationMode(LocationMode.Hight_Accuracy); // 设置定位模式
+    androidOption.setScanspan(1000); // 设置发起定位请求时间间隔
+
+    Map androidMap = androidOption.getMap();
+
+    /// ios 端设置定位参数
+    BaiduLocationIOSOption iosOption = new BaiduLocationIOSOption();
+    iosOption.setIsNeedNewVersionRgc(true); // 设置是否需要返回最新版本rgc信息
+    iosOption.setBMKLocationCoordinateType(
+        "BMKLocationCoordinateTypeBMK09LL"); // 设置返回的位置坐标系类型
+    iosOption.setActivityType("CLActivityTypeAutomotiveNavigation"); // 设置应用位置类型
+    iosOption.setLocationTimeout(10); // 设置位置获取超时时间
+    iosOption.setDesiredAccuracy("kCLLocationAccuracyBest"); // 设置预期精度参数
+    iosOption.setReGeocodeTimeout(10); // 设置获取地址信息超时时间
+    iosOption.setDistanceFilter(100); // 设置定位最小更新距离
+    iosOption.setAllowsBackgroundLocationUpdates(true); // 是否允许后台定位
+    iosOption.setPauseLocUpdateAutomatically(true); //  定位是否会被系统自动暂停
+
+    Map iosMap = iosOption.getMap();
+
+    _locationPlugin.prepareLoc(androidMap, iosMap);
+  }
+
+  /// 启动定位
+  void _startLocation() {
+    if (null != _locationPlugin) {
+      _setLocOption();
+      _locationPlugin.startLocation();
+    }
+  }
+
+  /// 停止定位
+  void _stopLocation() {
+    if (null != _locationPlugin) {
+      _locationPlugin.stopLocation();
+    }
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -82,8 +170,14 @@ class _HomeRouteState extends State<HomeRoute> {
 
   @override
   Widget build(BuildContext context) {
-//    /// 动态申请定位权限
-//    _locationPlugin.requestPermission();
+    if (_loationResult != null) {
+      _loationResult.forEach((key, value) {
+        print("key:$key");
+        print("value:$value");
+        showToast("定位成功");
+        _stopLocation();
+      });
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -224,10 +318,20 @@ class _HomeRouteState extends State<HomeRoute> {
               ),
               Divider(),
               SimpleDialogOption(
-                child: Text('Option C'),
+                child: Text('开始定位'),
                 onPressed: () {
-                  print('Option C');
-                  Navigator.pop(context, 'C');
+                  print('开始定位');
+                  Navigator.pop(context, '开始定位');
+                  _startLocation();
+                },
+              ),
+              Divider(),
+              SimpleDialogOption(
+                child: Text('停止定位'),
+                onPressed: () {
+                  print('停止定位');
+                  Navigator.pop(context, '停止定位');
+                  _stopLocation();
                 },
               ),
               Divider(),
